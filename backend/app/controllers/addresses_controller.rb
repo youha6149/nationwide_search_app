94f04777f2f0759f0a5efffeaf49603aed0fe012
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-09-21 19:46:08 +0900

    Fix address search pagination logic

--- backend/app/controllers/addresses_controller.rb
+++ backend/app/controllers/addresses_controller.rb
@@ -6,12 +6,18 @@ class AddressesController < ApplicationController
     end
 
     begin
+      page = params[:page].to_i > 0 ? params[:page].to_i : 1
+      per_page = params[:per_page].to_i > 0 ? params[:per_page].to_i : 10
+      from_value = (page - 1) * per_page
+
       results = Address.search({
         query: {
           match_phrase: {
             combined_field: params[:query]
           }
-        }
+        },
+        from: from_value,
+        size: per_page
       })
 
       render json: results.records, status: :ok
