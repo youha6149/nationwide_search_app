1ce5e8bb6b67ab3eea6de7152fb80914448a99fb
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-09-21 17:38:49 +0900

    Fix address controller spec to handle multiple search results

--- backend/spec/controllers/addresses_controller_spec.rb
+++ backend/spec/controllers/addresses_controller_spec.rb
@@ -15,25 +15,25 @@ RSpec.describe AddressesController, type: :controller do
     end
 
     context 'when search query is provided' do
-      let!(:address) { create(:address) }
+      let!(:address1) { create(:address) }
+      let!(:address2) { create(:address, :osaka) }
 
       before do
-        allow(Address).to receive(:search).and_return(double('response', records: [address]))
+        allow(Address).to receive(:search).and_return(double('response', records: [address1, address2]))
       end
 
-      it 'returns a success status and the correct search results' do
+      it 'returns a success status and the correct search results for multiple addresses' do
         get :search, params: valid_params
 
         expect(response).to have_http_status(:ok)
         parsed_body = JSON.parse(response.body)
 
-        # レスポンスが配列であることを確認
         expect(parsed_body).to be_an(Array)
+        expect(parsed_body.size).to eq(2)
 
-        # 最初のレコードに含まれるキーと値を確認
+        # 全てのフィールドをカバーする
         first_record = parsed_body.first
         expect(first_record).to include(
-          'id' => 456374,
           'address_cd' => 105000000,
           'prefecture_cd' => 13,
           'city_cd' => 13103,
@@ -45,6 +45,34 @@ RSpec.describe AddressesController, type: :controller do
           'prefecture_kana' => "トウキョウト",
           'city' => "港区",
           'city_kana' => "ミナトク",
+          'town' => nil,
+          'town_kana' => "　",
+          'town_supplement' => "（該当なし）",
+          'kyoto_street' => nil,
+          'chome' => nil,
+          'chome_kana' => nil,
+          'supplement' => nil,
+          'business_name' => nil,
+          'business_name_kana' => nil,
+          'business_address' => nil,
+          'new_address_cd' => nil,
+          'created_at' => "2024-09-15T08:20:55.666Z",
+          'updated_at' => "2024-09-15T08:20:55.666Z"
+        )
+
+        second_record = parsed_body.second
+        expect(second_record).to include(
+          'address_cd' => 123456789,
+          'prefecture_cd' => 27,
+          'city_cd' => 27100,
+          'town_cd' => 271000000,
+          'postal_code' => "530-0000",
+          'business_flag' => false,
+          'abolished_flag' => false,
+          'prefecture' => "大阪府",
+          'prefecture_kana' => "オオサカフ",
+          'city' => "大阪市",
+          'city_kana' => "オオサカシ",
           'town' => nil,
           'town_kana' => "　",
           'town_supplement' => "（該当なし）",
