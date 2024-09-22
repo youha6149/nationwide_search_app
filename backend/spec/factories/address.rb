8663673508331926cebcd13040cb55e13a9bf58f
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-09-22 11:24:22 +0900

    Add address factory traits

--- backend/spec/factories/address.rb
+++ backend/spec/factories/address.rb
@@ -23,6 +23,31 @@ FactoryBot.define do
     business_address { nil }
     new_address_cd { nil }
 
+    trait :shinagawa do
+      address_cd { 140000000 }
+      prefecture_cd { 13 }
+      city_cd { 13109 }
+      town_cd { 131090000 }
+      postal_code { "140-0000" }
+      business_flag { false }
+      abolished_flag { false }
+      prefecture { "東京都" }
+      prefecture_kana { "トウキョウト" }
+      city { "品川区" }
+      city_kana { "シナガワク" }
+      town { nil }
+      town_kana { "　" }
+      town_supplement { "（該当なし）" }
+      kyoto_street { nil }
+      chome { nil }
+      chome_kana { nil }
+      supplement { nil }
+      business_name { nil }
+      business_name_kana { nil }
+      business_address { nil }
+      new_address_cd { nil }
+    end
+
     trait :osaka do
       address_cd { 577000000 }
       prefecture_cd { 27 }
