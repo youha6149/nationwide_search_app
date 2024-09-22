6dbebe54b05238d19e64938c87a896c9a902a466
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-09-22 16:45:46 +0900

    Fix navigation links in App.vue for address search functionality

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,27 +1,12 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import AddressSearch from '../components/AddressSearch.vue';
+import AddressSearch from "../components/AddressSearch.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: '/search',
-    name: 'Search',
+    name: "Search",
     component: AddressSearch,
-  }
+  },
 ];
 
 const router = createRouter({
