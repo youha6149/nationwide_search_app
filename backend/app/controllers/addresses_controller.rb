e693a69ce10818808695523cac53b6f461372739
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-09-23 11:36:29 +0900

    Add create in addresses controller

--- backend/app/controllers/addresses_controller.rb
+++ backend/app/controllers/addresses_controller.rb
@@ -27,4 +27,22 @@ class AddressesController < ApplicationController
       render json: { error: "検索に失敗しました: #{e.message}" }, status: :unprocessable_entity
     end
   end
+
+  def create
+    begin
+      csv_fetcher = CsvFetcherService.new
+      csv_file_path = csv_fetcher.extract_csv_from_zip
+
+      Address.transaction do
+        Address.import_from_csv(csv_file_path)
+        Address.ensure_index_exists
+      end
+
+      render json: { message: "CSVの取り込みが完了しました" }, status: :ok
+    ensure
+      File.delete(csv_file_path) if csv_file_path && File.exist?(csv_file_path)
+    end
+  rescue => e
+    render json: { error: "CSVの取り込みに失敗しました: #{e.message}" }, status: :unprocessable_entity
+  end
 end
