9a3eeb741bd4bf537edb717d6c00c34dd7d6857f
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-10-11 17:41:44 +0900

    Add check_headers method to extract_csv_from_zip

--- backend/app/services/csv_fetcher_service.rb
+++ backend/app/services/csv_fetcher_service.rb
@@ -20,6 +20,7 @@ class CsvFetcherService
   def extract_csv_from_zip
     extracted_csv_path = extract_csv(@zip_path)
     check_encoding(extracted_csv_path)
+    check_headers(extracted_csv_path)
     extracted_csv_path
   rescue => e
     Rails.logger.error("Failed to extract CSV from zip: #{e.message}")
