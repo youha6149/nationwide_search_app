b8a06a2f6306ae5747ef573f98d348ea9ea441b5
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-10-19 12:08:48 +0900

    Fix HomeView component to use AddressSearch instead of HelloWorld

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <AddressSearch />
+    <!-- 以下にこのWebアプリの特徴などを書く -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import AddressSearch from "@/components/AddressSearch.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    AddressSearch,
   },
 });
 </script>
