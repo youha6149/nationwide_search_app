c7f078e4e0c188ad8bc0fe923fee94d127319472
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-10-20 11:07:29 +0900

    Add ResultsView component to router config

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import ResultsView from "../views/ResultsView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: HomeView,
+  },
+  {
+    path: "/results",
+    name: "Results",
+    component: ResultsView,
   },
 ];
 
