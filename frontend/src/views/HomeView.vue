9db289a01711568cc5057072f6aa9c46b8874bb0
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-10-25 19:56:12 +0900

    Update PDF viewer URL  in HomeView.vue

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -38,8 +38,10 @@
     <div class="pdf-viewer">
       <div class="iframe-container">
         <iframe
-          src="https://docs.google.com/presentation/d/e/2PACX-1vSaWxsLKSEcHv-eb4UgT_QUDcfvmg5SPD3Pp4uituLe9EYO2WXrg6JIum1HkYonIFV1mgGjKF4TLXVT/embed?start=false&loop=false&delayms=3000"
+          src="https://docs.google.com/presentation/d/e/2PACX-1vSR9GbLD-22P94Yy7U7VrZmtSCgoFpbLmbwpUstvFzGs21ZEJf3Sbjht2z4CQpmR6oh2GqrZumHaTPE/embed?start=false&loop=false&delayms=3000"
           frameborder="0"
+          width="960"
+          height="749"
           allowfullscreen="true"
           mozallowfullscreen="true"
           webkitallowfullscreen="true"
