901cbc87f4ec68bfde21c09a28bd4bffd0788f71
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-10-26 17:36:44 +0900

    Move class "pdf-viewer" in HomeView to PdfViewer

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,19 +2,7 @@
   <div class="home">
     <AddressSearch />
     <AppFeatures />
-    <div class="pdf-viewer">
-      <div class="iframe-container">
-        <iframe
-          src="https://docs.google.com/presentation/d/e/2PACX-1vSR9GbLD-22P94Yy7U7VrZmtSCgoFpbLmbwpUstvFzGs21ZEJf3Sbjht2z4CQpmR6oh2GqrZumHaTPE/embed?start=false&loop=false&delayms=3000"
-          frameborder="0"
-          width="960"
-          height="749"
-          allowfullscreen="true"
-          mozallowfullscreen="true"
-          webkitallowfullscreen="true"
-        ></iframe>
-      </div>
-    </div>
+    <PdfViewer />
   </div>
 </template>
 
@@ -22,38 +10,19 @@
 import { defineComponent } from "vue";
 import AddressSearch from "@/components/AddressSearch.vue";
 import AppFeatures from "@/components/AppFeatures.vue";
+import PdfViewer from "@/components/PdfViewer.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     AddressSearch,
     AppFeatures,
+    PdfViewer,
   },
 });
 </script>
 
 <style scoped>
-.pdf-viewer {
-  width: 100%;
-  max-width: 800px;
-  margin: 20px auto;
-}
-
-.iframe-container {
-  position: relative;
-  width: 100%;
-  padding-top: 56.25%;
-}
-
-.iframe-container iframe {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: 0;
-}
-
 @media (max-width: 1200px) {
   .features {
     grid-template-columns: repeat(2, 1fr);
