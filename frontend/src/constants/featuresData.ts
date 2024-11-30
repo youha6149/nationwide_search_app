efd4c1518d2a67438aabb21773ac36c286b01f86
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-11-30 17:11:45 +0900

    Fix image import method from require to import statement

--- frontend/src/constants/featuresData.ts
+++ frontend/src/constants/featuresData.ts
@@ -5,31 +5,36 @@ export interface FeatureCard {
   description: string;
 }
 
+import IconCPU from "@/assets/icon_cpu.svg";
+import IconKey from "@/assets/icon_key.svg";
+import IconPuzzle from "@/assets/icon_puzzle.svg";
+import IconTag from "@/assets/icon_tag.svg";
+
 export const features: FeatureCard[] = [
   {
     title: "パフォーマンス",
-    image: require("@/assets/icon_cpu.svg"),
+    image: IconCPU,
     alt: "CPU",
     description:
       "ElasticSearchによる超高速な検索機能、Railsの強力なAPI、Vueの軽量で直感的なUI、そしてMySQLの堅牢なデータ処理を組み合わせ、膨大なデータでも迅速かつ確実に処理。",
   },
   {
     title: "セキュリティ",
-    image: require("@/assets/icon_key.svg"),
+    image: IconKey,
     alt: "Key",
     description:
       "Railsの強固なセキュリティ機能を標準装備し、CSRFやSQLインジェクションにも対応。安心してお使いいただける堅牢なセキュリティ体制を提供します。",
   },
   {
     title: "拡張性",
-    image: require("@/assets/icon_puzzle.svg"),
+    image: IconPuzzle,
     alt: "Puzzle",
     description:
       "バックエンドにRails、フロントエンドにVueを採用し、Elasticsearchのスケーラビリティを活かすことで、システムの成長に合わせた柔軟な拡張性を確保。",
   },
   {
     title: "保守性",
-    image: require("@/assets/icon_tag.svg"),
+    image: IconTag,
     alt: "Tag",
     description:
       "TDDやRSpec、TypeScript、Dockerを導入し、高品質なコードを維持しつつ、開発環境の再現性を高め、保守性に優れたシステムを提供。",
